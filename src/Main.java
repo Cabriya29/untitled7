86cd89d5a24353464cc88ac766e7abcc4e5f760d
PRAYON FERNANDO <prayon@creo360.net>
2024-05-20 15:42:40 +0530

    my first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        softwareEngineering obj1 = new softwareEngineering("tara","java","construction");
+        softwareEngineering obj2 = new softwareEngineering("aziz","python","hospital management system");
+
+        System.out.println(obj1.name);
+        obj1.coding();
+
+    }
+
+
+}
---
+++ src/softwareEngineering.java
@@ -0,0 +1,19 @@
+public class softwareEngineering {
+    String name;
+    String programminglanguageExpertise;
+    String favouriteCodingProject;
+
+
+    softwareEngineering(String name,String programminglanguageExpertise,String favouriteCodingProject) {
+        this.name=name;
+        this.programminglanguageExpertise=programminglanguageExpertise;
+        this.favouriteCodingProject=favouriteCodingProject;
+
+    }
+
+    //syntax of method= return type + method name +()+{}
+
+    void coding(){}
+
+    void debugging(){}
+}
